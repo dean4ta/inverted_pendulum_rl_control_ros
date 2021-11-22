57759a8ddb752c2be3f6c0aae86fee2512fc9089
Dean Fortier <dean4ta@gmail.com>
2021-11-21 22:33:23 -0800

    adding option for position control

--- inverted_pendulum_rl_control/scripts/train_ddpg.py
+++ inverted_pendulum_rl_control/scripts/train_ddpg.py
@@ -5,6 +5,7 @@ from rl_common.ddpg import Agent
 from sensor_msgs.msg import JointState
 from std_msgs.msg import Float64, Float64MultiArray
 from gazebo_msgs.msg import ModelStates
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 
 from std_srvs.srv import Empty, EmptyRequest
 from inverted_pendulum_rl_control.srv import (
@@ -32,18 +33,12 @@ class TrainDDPG:
     def __init__(self):
         # TODO: parameterize the namespace
         rospy.loginfo("Initializing DDPG")
-        self.joint_sub = rospy.Subscriber(
-            "/joint_states", JointState, self.joint_callback
-        )
-        self.model_state_sub = rospy.Subscriber(
-            "/gazebo/model_states", ModelStates, self.model_state_callback
-        )
-        self.init_joint_publishers()
-        self.reward_pub = rospy.Publisher("/reward", Float64, queue_size=10)
-        self.state_pub = rospy.Publisher("/state", Float64MultiArray, queue_size=10)
-        self.save_model_srv = rospy.Service("save_model", SaveModel, self.save_model)
 
-        self.standing_joints = rospy.get_param("standing_joint_position")
+        self.init_parameters()
+        self.init_subscribers()
+        self.init_publishers()
+
+        self.save_model_srv = rospy.Service("save_model", SaveModel, self.save_model)
 
         self.wait_for_services()
         self.reset()
@@ -53,43 +48,57 @@ class TrainDDPG:
         rospy.loginfo("Beginning Training")
         self.train()
 
+    def init_parameters(self):
+        self.joint_names = rospy.get_param("/joint_names")
+        self.standing_joint_position = rospy.get_param("standing_joint_position")
+        self.standing_joint_effort = rospy.get_param("standing_joint_effort")
+        self.start_episode_standing = rospy.get_param("start_episode_standing")
+        self.use_effort = rospy.get_param("use_effort")
+        self.expert_system_command_topic = rospy.get_param(
+            "champ_controller/joint_controller_topic"
+        )
+
+    def init_subscribers(self):
+        self.joint_sub = rospy.Subscriber(
+            "/joint_states", JointState, self.joint_callback
+        )
+        self.model_state_sub = rospy.Subscriber(
+            "/gazebo/model_states", ModelStates, self.model_state_callback
+        )
+        self.expert_system_command_sub = rospy.Subscriber(
+            self.expert_system_command_topic,
+            JointTrajectory,
+            self.expert_system_callback,
+        )
+
+    def init_publishers(self):
+        self.init_joint_publishers()
+        self.reward_pub = rospy.Publisher("/reward", Float64, queue_size=10)
+        self.state_pub = rospy.Publisher("/state", Float64MultiArray, queue_size=10)
+
     def init_joint_publishers(self):
-        self.LF_HAA_pub = rospy.Publisher(
-            "/joint_LF_HAA_effort_controller/command", Float64, queue_size=10
-        )
-        self.LF_HFE_pub = rospy.Publisher(
-            "/joint_LF_HFE_effort_controller/command", Float64, queue_size=10
-        )
-        self.LF_KFE_pub = rospy.Publisher(
-            "/joint_LF_KFE_effort_controller/command", Float64, queue_size=10
-        )
-        self.LH_HAA_pub = rospy.Publisher(
-            "/joint_LH_HAA_effort_controller/command", Float64, queue_size=10
-        )
-        self.LH_HFE_pub = rospy.Publisher(
-            "/joint_LH_HFE_effort_controller/command", Float64, queue_size=10
-        )
-        self.LH_KFE_pub = rospy.Publisher(
-            "/joint_LH_KFE_effort_controller/command", Float64, queue_size=10
-        )
-        self.RF_HAA_pub = rospy.Publisher(
-            "/joint_RF_HAA_effort_controller/command", Float64, queue_size=10
-        )
-        self.RF_HFE_pub = rospy.Publisher(
-            "/joint_RF_HFE_effort_controller/command", Float64, queue_size=10
-        )
-        self.RF_KFE_pub = rospy.Publisher(
-            "/joint_RF_KFE_effort_controller/command", Float64, queue_size=10
-        )
-        self.RH_HAA_pub = rospy.Publisher(
-            "/joint_RH_HAA_effort_controller/command", Float64, queue_size=10
-        )
-        self.RH_HFE_pub = rospy.Publisher(
-            "/joint_RH_HFE_effort_controller/command", Float64, queue_size=10
-        )
-        self.RH_KFE_pub = rospy.Publisher(
-            "/joint_RH_KFE_effort_controller/command", Float64, queue_size=10
-        )
+        if self.use_effort:
+            # fmt: off
+            self.LF_HAA_pub = rospy.Publisher("/joint_LF_HAA_effort_controller/command", Float64, queue_size=10)
+            self.LF_HFE_pub = rospy.Publisher("/joint_LF_HFE_effort_controller/command", Float64, queue_size=10)
+            self.LF_KFE_pub = rospy.Publisher("/joint_LF_KFE_effort_controller/command", Float64, queue_size=10)
+            self.LH_HAA_pub = rospy.Publisher("/joint_LH_HAA_effort_controller/command", Float64, queue_size=10)
+            self.LH_HFE_pub = rospy.Publisher("/joint_LH_HFE_effort_controller/command", Float64, queue_size=10)
+            self.LH_KFE_pub = rospy.Publisher("/joint_LH_KFE_effort_controller/command", Float64, queue_size=10)
+            self.RF_HAA_pub = rospy.Publisher("/joint_RF_HAA_effort_controller/command", Float64, queue_size=10)
+            self.RF_HFE_pub = rospy.Publisher("/joint_RF_HFE_effort_controller/command", Float64, queue_size=10)
+            self.RF_KFE_pub = rospy.Publisher("/joint_RF_KFE_effort_controller/command", Float64, queue_size=10)
+            self.RH_HAA_pub = rospy.Publisher("/joint_RH_HAA_effort_controller/command", Float64, queue_size=10)
+            self.RH_HFE_pub = rospy.Publisher("/joint_RH_HFE_effort_controller/command", Float64, queue_size=10)
+            self.RH_KFE_pub = rospy.Publisher("/joint_RH_KFE_effort_controller/command", Float64, queue_size=10)
+            # fmt: on
+        else:
+            self.joint_position_pub = rospy.Publisher(
+                "/joint_group_position_controller/command",
+                JointTrajectory,
+                queue_size=10,
+            )
+            self.joint_seq = 0
 
     def wait_for_services(self):
         rospy.wait_for_service("/gazebo/pause_physics")
@@ -108,6 +117,9 @@ class TrainDDPG:
         self.model_state_pose = msg.pose[1]
         self.model_state_twist = msg.twist[1]
 
+    def expert_system_callback(self, msg):
+        self.expert_system_command = msg.points[0].positions
+
     def angle_normalize(self, angle):
         # Normalize angle to -pi to pi
         return (angle + np.pi) % (2 * np.pi) - np.pi
@@ -118,18 +130,29 @@ class TrainDDPG:
         self.state_pub.publish(msg)
 
     def publish_action(self, action):
-        self.LF_HAA_pub.publish(Float64(action[0]))
-        self.LF_HFE_pub.publish(Float64(action[1]))
-        self.LF_KFE_pub.publish(Float64(action[2]))
-        self.LH_HAA_pub.publish(Float64(action[3]))
-        self.LH_HFE_pub.publish(Float64(action[4]))
-        self.LH_KFE_pub.publish(Float64(action[5]))
-        self.RF_HAA_pub.publish(Float64(action[6]))
-        self.RF_HFE_pub.publish(Float64(action[7]))
-        self.RF_KFE_pub.publish(Float64(action[8]))
-        self.RH_HAA_pub.publish(Float64(action[9]))
-        self.RH_HFE_pub.publish(Float64(action[10]))
-        self.RH_KFE_pub.publish(Float64(action[11]))
+        if self.use_effort:
+            self.LF_HAA_pub.publish(Float64(action[0]))
+            self.LF_HFE_pub.publish(Float64(action[1]))
+            self.LF_KFE_pub.publish(Float64(action[2]))
+            self.LH_HAA_pub.publish(Float64(action[3]))
+            self.LH_HFE_pub.publish(Float64(action[4]))
+            self.LH_KFE_pub.publish(Float64(action[5]))
+            self.RF_HAA_pub.publish(Float64(action[6]))
+            self.RF_HFE_pub.publish(Float64(action[7]))
+            self.RF_KFE_pub.publish(Float64(action[8]))
+            self.RH_HAA_pub.publish(Float64(action[9]))
+            self.RH_HFE_pub.publish(Float64(action[10]))
+            self.RH_KFE_pub.publish(Float64(action[11]))
+        else:
+            joint_trajectory = JointTrajectory()
+            joint_trajectory.header.seq = self.joint_seq
+            self.joint_seq += 1
+            joint_trajectory.joint_names = self.joint_names
+            point = JointTrajectoryPoint()
+            point.positions = action
+            joint_trajectory.points.append(point)
+            joint_trajectory.header.stamp = rospy.Time.now() + rospy.Duration(0.01)
+            self.joint_position_pub.publish(joint_trajectory)
 
     def reset(self):
         # Reset performs the following actions:
@@ -156,23 +179,15 @@ class TrainDDPG:
         req = SetModelConfigurationRequest()
         req.model_name = "/"
         req.urdf_param_name = "robot_description"
-        req.joint_names = [
-            "LF_HAA",
-            "LF_HFE",
-            "LF_KFE",
-            "LH_HAA",
-            "LH_HFE",
-            "LH_KFE",
-            "RF_HAA",
-            "RF_HFE",
-            "RF_KFE",
-            "RH_HAA",
-            "RH_HFE",
-            "RH_KFE",
-        ]
-        joint_positions = np.random.uniform(-9.42477796077, 9.42477796077, (4, 3))
-        joint_positions[:, 0] = np.random.uniform(-0.72, 0.49, (4,))
-        joint_positions = joint_positions.flatten()
+        req.joint_names = self.joint_names
+        if not self.start_episode_standing:
+            joint_positions = np.random.uniform(-9.42477796077, 9.42477796077, (4, 3))
+            joint_positions[:, 0] = np.random.uniform(-0.72, 0.49, (4,))
+            joint_positions = joint_positions.flatten()
+        else:
+            joint_positions = self.standing_joint_position
+            # joint_positions += np.random.normal(0, 0.01, (N_JOINTS,))
+
         req.joint_positions = joint_positions
         try:
             resp = rospy.ServiceProxy(
@@ -190,16 +205,29 @@ class TrainDDPG:
         except rospy.ServiceException as e:
             rospy.logwarn("/gazebo/unpause_physics service call failed: {0}".format(e))
 
-        self.publish_action(
-            np.random.uniform(-MAX_RAND_Torque, MAX_RAND_Torque, (N_JOINTS,))
-        )
+        if not self.start_episode_standing:
+            joint_efforts = np.random.uniform(
+                -MAX_RAND_Torque, MAX_RAND_Torque, (N_JOINTS,)
+            )
+        else:
+            joint_efforts = self.standing_joint_effort
+            joint_efforts += np.random.normal(0, 0.5, (N_JOINTS,))
+
+        self.publish_action(joint_efforts)
 
     def get_reward(self, state_next):
-        reward = 0
-        for joint in range(N_JOINTS):
-            reward += np.exp(-np.abs(state_next[joint] - self.standing_joints[joint]))
-        self.reward_pub.publish(Float64(reward))
-        return reward
+        if not self.start_episode_standing:
+            reward = 0
+            for joint in range(N_JOINTS):
+                reward += np.exp(
+                    -np.abs(state_next[joint] - self.standing_joint_position[joint])
+                )
+            self.reward_pub.publish(Float64(reward))
+            return reward
+        else:
+            reward = self.model_state_pose.position.z * 10
+            self.reward_pub.publish(Float64(reward))
+            return reward
 
     def save_model(self, req):
         rospack = rospkg.RosPack()
@@ -246,7 +274,11 @@ class TrainDDPG:
             while t < 200:
                 if rospy.is_shutdown():
                     return
-                action = self.agent.select_action(state)[0]
+
+                if t > 20:
+                    action = self.agent.select_action(state)[0]
+                else:
+                    action = self.expert_system_command
 
                 self.publish_action(action)
                 self.publish_state(state)
@@ -258,7 +290,7 @@ class TrainDDPG:
                     self.is_state_ready = False
                 state_next = self.state
                 reward = self.get_reward(state_next)
-                if t < 5:  # first few rewards seem to be random so we ignore them
+                if t < 2:  # first few rewards seem to be random so we ignore them
                     reward = 0
                 score += reward
 
